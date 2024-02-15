859ab4d0b8122cbe7a0f4604242b5ee6b84338e5
bej <bej@systematic.com>
2024-02-15 14:50:22 +0100

    Constructor reflection in the works

--- src/main/java/com/bitcrunzh/generic/config/reflection/ClassDescriptionFactory.java
+++ src/main/java/com/bitcrunzh/generic/config/reflection/ClassDescriptionFactory.java
@@ -5,13 +5,13 @@ import com.bitcrunzh.generic.config.description.java.ClassDescriptionCache;
 import com.bitcrunzh.generic.config.description.java.PropertyDescription;
 import com.bitcrunzh.generic.config.description.java.Version;
 import com.bitcrunzh.generic.config.reflection.annotation.ConfigurationModel;
-import com.bitcrunzh.generic.config.reflection.annotation.util.ConstructParamFieldMapping;
 import com.bitcrunzh.generic.config.validation.ObjectValidator;
 import com.bitcrunzh.generic.config.value.java.NormalizedObject;
+import com.bitcrunzh.generic.config.value.java.NormalizedProperty;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
+import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 import java.util.function.Function;
 
@@ -21,7 +21,7 @@ public class ClassDescriptionFactory {
         Version modeVersion = getModelVersion(annotation);
         ClassDescriptionCache classDescriptionCache = new ClassDescriptionCache();
         List<PropertyDescription<T, ?>> propertyDescriptions = createPropertyDescriptions(type, classDescriptionCache);
-        Function<NormalizedObject<T>, T> constructorFunction = createConstructorFunction(propertyDescriptions, type);
+        Function<NormalizedObject<T>, T> constructorFunction = createConstructorFunction(propertyDescriptions, type, classDescriptionCache);
         ObjectValidator<T> validator = createValidator(type, annotation);
         return new ClassDescription<>(type, modeVersion, propertyDescriptions, constructorFunction, validator);
     }
@@ -35,36 +35,83 @@ public class ClassDescriptionFactory {
         }
     }
 
-    private static <T> Function<NormalizedObject<T>, T> createConstructorFunction(List<PropertyDescription<T, ?>> propertyDescriptions, Class<T> type) {
-        Set<String> propertiesWithNoSetter = Collections.unmodifiableSet(filterPropertiesWithNoSetter(propertyDescriptions));
-        for (Constructor<?> constructor : type.getConstructors()) {
-            Set<String> propertiesRequiredInConstructor = new HashSet<>(propertiesWithNoSetter);
-            Set<String> propertiesToUseSetter = getPropertyNames(propertyDescriptions);
-            List<String> propertiesToUseConstructor = new ArrayList<>();
-            boolean isPublic = Modifier.isPublic(constructor.getModifiers());
-            boolean isEmptyConstructor = constructor.getParameters() == null || constructor.getParameters().length == 0;
-            if (
-                    !isPublic ||
-                            (isEmptyConstructor && !propertiesRequiredInConstructor.isEmpty())
-
-            ) {
-                //Not usable constructor
-                continue;
-            }
-            if(isEmptyConstructor && propertiesRequiredInConstructor.isEmpty()) {
-                return createConstructorFunction(propertiesToUseSetter, propertiesToUseConstructor, propertyDescriptions, constructor);
-            }
-                ConstructParamFieldMapping paramFieldMapping = constructor.getAnnotation(ConstructParamFieldMapping.class);
+    private static <T> Function<NormalizedObject<T>, T> createConstructorFunction(List<PropertyDescription<T, ?>> propertyDescriptions, Class<T> type, ClassDescriptionCache classDescriptionCache) {
+        ReflectionUtil.ConstructorMatch<T> constructorMatch = ReflectionUtil.findconstructor(propertyDescriptions, type);
+        if (!constructorMatch.isConstructorFound()) {
+            throw new IllegalArgumentException(String.format("Unable to find appropriate constructor for instantiating all properties for type '%s'. Constructors investigated:", type.getSimpleName(), printConstructors(constructorMatch.getNotMatchingConstructors())));
         }
-        return null;
+        return createConstructorFunctionFromMatch(constructorMatch, propertyDescriptions, type, classDescriptionCache);
     }
 
-    private static <T> Set<String> getPropertyNames(List<PropertyDescription<T,?>> propertyDescriptions) {
-        return null;
+    private static String printConstructors(List<String> notMatchingConstructors) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String notMatchingConstructor : notMatchingConstructors) {
+            stringBuilder.append("%n").append(notMatchingConstructor);
+        }
+        return stringBuilder.toString();
     }
 
-    private static <T> Set<String> filterPropertiesWithNoSetter(List<PropertyDescription<T,?>> propertyDescriptions) {
-        return null;
+    private static <T> Function<NormalizedObject<T>, T> createConstructorFunctionFromMatch(ReflectionUtil.ConstructorMatch<T> constructorMatch, List<PropertyDescription<T, ?>> propertyDescriptions, Class<T> type, ClassDescriptionCache classDescriptionCache) {
+        Map<String, PropertyDescription<T, ?>> propertyDescriptionMap = new HashMap<>();
+        for (PropertyDescription<T, ?> propertyDescription : propertyDescriptions) {
+            propertyDescriptionMap.put(propertyDescription.getPropertyName(), propertyDescription);
+        }
+        return normalizedObject -> {
+            Object[] constructorArgs = getConstructorArgs(normalizedObject, constructorMatch, classDescriptionCache);
+            try {
+                T object = constructorMatch.getConstructor().newInstance(constructorArgs);
+                for (PropertyDescription<T, ?> propertyDescription : constructorMatch.getSetterProperties().values()) {
+                    NormalizedProperty<?> normalizedPropertyValue = normalizedObject.getProperty(propertyDescription.getPropertyName());
+                    //noinspection unchecked,CastCanBeRemovedNarrowingVariableType
+                    setProperty(object, (NormalizedProperty<Object>) normalizedPropertyValue, (PropertyDescription<T, Object>) propertyDescription, classDescriptionCache);
+                }
+                return object;
+            } catch (InstantiationException e) {
+                throw new IllegalStateException("Failed to instantiate object.", e);
+            } catch (IllegalAccessException e) {
+                throw new IllegalStateException("Failed to instantiate object. Constructor must be public.", e);
+            } catch (InvocationTargetException e) {
+                throw new IllegalStateException("Failed to instantiate object. Wrong invocation target.", e);
+            }
+        };
+    }
+
+    private static <T, V> Object[] getConstructorArgs(NormalizedObject<T> normalizedObject, ReflectionUtil.ConstructorMatch<T> constructorMatch, ClassDescriptionCache classDescriptionCache) {
+        Object[] constructorParameters = new Object[constructorMatch.getConstructorParameters().size()];
+        for (int i = 0; i < constructorMatch.getConstructorParameters().size(); i++) {
+            @SuppressWarnings("unchecked") PropertyDescription<T, V> propertyDescription = (PropertyDescription<T, V>) constructorMatch.getConstructorParameters().get(i);
+            NormalizedProperty<V> normalizedProperty = normalizedObject.getProperty(propertyDescription.getPropertyName());
+            V property = getProperty(normalizedProperty, propertyDescription, classDescriptionCache);
+            constructorParameters[i] = property;
+        }
+        return constructorParameters;
+    }
+
+    private static <T, V> V getProperty(NormalizedProperty<V> normalizedPropertyValue, PropertyDescription<T, V> propertyDescription, ClassDescriptionCache classDescriptionCache) {
+        return propertyDescription.createProperty(normalizedPropertyValue, classDescriptionCache).orElse(null);
+    }
+
+    private static <T, V> void setProperty(T object, NormalizedProperty<V> normalizedPropertyValue, PropertyDescription<T, V> propertyDescription, ClassDescriptionCache classDescriptionCache) {
+        V value = getProperty(normalizedPropertyValue, propertyDescription, classDescriptionCache);
+        propertyDescription.getSetterFunction().orElseThrow(() -> new IllegalStateException("No setter function for property " + normalizedPropertyValue.getPropertyName())).accept(object, value);
+    }
+
+    private static <T> Set<String> getPropertyNames(List<PropertyDescription<T, ?>> propertyDescriptions) {
+        Set<String> propertyNames = new HashSet<>();
+        for (PropertyDescription<T, ?> propertyDescription : propertyDescriptions) {
+            propertyNames.add(propertyDescription.getPropertyName());
+        }
+        return propertyNames;
+    }
+
+    private static <T> Set<String> filterPropertiesWithNoSetter(List<PropertyDescription<T, ?>> propertyDescriptions) {
+        Set<String> propertyNames = new HashSet<>();
+        for (PropertyDescription<T, ?> propertyDescription : propertyDescriptions) {
+            if (!propertyDescription.getSetterFunction().isPresent()) {
+                propertyNames.add(propertyDescription.getPropertyName());
+            }
+        }
+        return propertyNames;
     }
 
     private static <T> Function<NormalizedObject<T>, T> createConstructorFunction(Set<String> propertiesToUseSetter, List<String> propertiesToUseConstructor, List<PropertyDescription<T, ?>> propertyDescriptions, Constructor<?> constructor) {
--- src/main/java/com/bitcrunzh/generic/config/value/java/NormalizedObject.java
+++ src/main/java/com/bitcrunzh/generic/config/value/java/NormalizedObject.java
@@ -3,15 +3,19 @@ package com.bitcrunzh.generic.config.value.java;
 import com.bitcrunzh.generic.config.description.java.Version;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NormalizedObject<T> implements Value {
     private final Class<T> type;
-    private final Collection<NormalizedProperty<?>> properties;
+    private final Map<String, NormalizedProperty<?>> propertyMap = new HashMap<>();
     private final Version modelVersion;
 
     public NormalizedObject(Class<T> type, Collection<NormalizedProperty<?>> properties, Version modelVersion) {
         this.type = type;
-        this.properties = properties;
+        for(NormalizedProperty<?> property : properties) {
+            propertyMap.put(property.getPropertyName(), property);
+        }
         this.modelVersion = modelVersion;
     }
 
@@ -20,7 +24,12 @@ public class NormalizedObject<T> implements Value {
     }
 
     public Collection<NormalizedProperty<?>> getProperties() {
-        return properties;
+        return propertyMap.values();
+    }
+
+    @SuppressWarnings("unchecked")
+    public <V> NormalizedProperty<V> getProperty(String propertyName) {
+        return (NormalizedProperty<V>) propertyMap.get(propertyName);
     }
 
     public Version getModelVersion() {
